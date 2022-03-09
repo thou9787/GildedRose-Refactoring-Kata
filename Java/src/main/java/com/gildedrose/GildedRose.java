0373c5c4d4f3378cad68ba81217793f6660a35bd
Yao-Siang Su <yaosiang.su@104.com.tw>
2022-03-09 17:58:48 +0800

    抽取方法 increaseQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,23 +13,14 @@ class GildedRose {
                 continue;
             }
             if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                increaseQuality(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                increaseQuality(item);
                 if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 }
-
                 if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 }
             } else {
                 if (item.quality > 0) {
@@ -41,9 +32,7 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 } else {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = item.quality - item.quality;
@@ -54,6 +43,12 @@ class GildedRose {
                     }
                 }
             }
+        }
+    }
+
+    protected void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
